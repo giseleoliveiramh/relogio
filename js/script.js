e107f9d91b493815cafe6a3424aaf060d2ded51a
giseleoliveiramh <111704659+giseleoliveiramh@users.noreply.github.com>
2022-08-31 19:16:10 -0300

    atualizei o relógio

--- js/script.js
+++ js/script.js
@@ -6,6 +6,16 @@ function relogio() {
     var min = data.getMinutes()
     var seg = data.getSeconds()
     var hora = `${hor}:${min}:${seg}`
+    if (hor < 10) {
+        hora = `0${hor}:${min}:${seg}`
+    }
+    if (min < 10) {
+        hora = `${hor}:0${min}:${seg}`
+    }
+    if (seg < 10) {
+        hora = `${hor}:${min}:0${seg}`
+    }
+    
     document.querySelector('input#relogio').value = `${hora}`
 }
 
